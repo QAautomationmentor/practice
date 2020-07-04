d9999c36fdf4ca383bd6c0fe8b970afdd4128a6b
autom <sisodidyaaditya95@gmail.com>
2020-07-05 04:01:04 +0530

    fifth commit

---
+++ src/test/java/testcases/SpicejetApp.java
@@ -0,0 +1,29 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import utilities.Base;
+
+public class SpicejetApp extends Base {
+	public WebDriver driver;
+
+	@BeforeClass
+	public void tearUp() {
+		driver = initializeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+	}
+
+	@Test(priority = 1)
+	public void openApplication() {
+		driver.get("https://www.spicejet.com/");
+	}
+}
