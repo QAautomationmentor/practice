9dfbc8605a90dca16e4909c4c0bb530121716756
autom <sisodidyaaditya95@gmail.com>
2020-07-06 16:38:48 +0530

    aisazone guy

---
+++ src/test/java/testcases/SpicejetApp.java
@@ -0,0 +1,27 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import utilities.Base;
+
+public class SpicejetApp extends Base {
+	public WebDriver driver;
+
+	@BeforeClass
+	public void tearUp() {
+		driver = initializeDriver();
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+	}
+
+	@Test(priority = 1)
+	public void openApplication() {
+		driver.get("https://www.spicejet.com/");
+	}
+}
