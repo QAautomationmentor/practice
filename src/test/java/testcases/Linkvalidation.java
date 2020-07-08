1e343de759720b4f9ab7eafa9c4a3317361bf267
aditya <adityasisodiya786@gmail.com>
2020-07-08 16:17:28 +0530

    australian guy

---
+++ src/test/java/testcases/Linkvalidation.java
@@ -0,0 +1,36 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import utilities.Base;
+
+public class Linkvalidation extends Base {
+	public WebDriver driver;
+
+	@BeforeClass
+	public void tearUp() {
+		driver = initializeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+	}
+
+	@Test(priority = 1)
+	public void openApplication() {
+		driver.get("https://www.rahulshettyacademy.com/");
+	}
+
+	@Test(priority = 2)
+	public void printInfo() {
+		System.out.println("Title : " + driver.getTitle());
+	}
+
+}
